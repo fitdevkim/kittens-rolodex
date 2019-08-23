dd3fe2ee19d5cc3bbbb63eba2b8a26ac77dcf8c4
Muhammad Nurhakim Bin Margono <hakimzdope@gmail,com>
2019-08-23 20:26:01 +0800

    initial commit

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './card.styles.css';
+
+export const Card = props => (
+  <div className="card-container">
+    <img
+      src={`https://robohash.org/${props.cat.id}?set=set4&size=180x180`}
+      alt="cat"
+    />
+    <h2>{props.cat.name}</h2>
+    <p>{props.cat.email}</p>
+  </div>
+);
