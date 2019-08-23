ef6f50fd8903d9af6d41ec8d05996bfaf2efa60a
Muhammad Nurhakim Bin Margono <hakimzdope@gmail,com>
2019-08-23 20:32:52 +0800

    Updates

---
+++ precache-manifest.81856113f3ed281618be2d782f1215f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c268e7f98ddbcda19025ae657184b9c1",
+    "url": "/kittens-rolodex/index.html"
+  },
+  {
+    "revision": "f4432b424151db1af6cc",
+    "url": "/kittens-rolodex/static/css/main.3662f0a8.chunk.css"
+  },
+  {
+    "revision": "824751268988c45e44c0",
+    "url": "/kittens-rolodex/static/js/2.07e7c8cd.chunk.js"
+  },
+  {
+    "revision": "f4432b424151db1af6cc",
+    "url": "/kittens-rolodex/static/js/main.48deec0c.chunk.js"
+  },
+  {
+    "revision": "4c42f657f22d309eacb6",
+    "url": "/kittens-rolodex/static/js/runtime~main.ef78b9ac.js"
+  }
+]);
